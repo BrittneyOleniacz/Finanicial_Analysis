99351a306c47b2fcea8af4604873452b77e63d4f
Brittney Oleniacz <brittneyoleniacz@gmail.com>
2020-08-12 23:28:47 -0500

    HW is done. Time for bed.

---
+++ PyBank/main.py
@@ -0,0 +1,60 @@
+import os
+import csv
+import sys  
+
+with open("budget_data.csv") as csvfile:
+    csvreader = csv.DictReader(csvfile)
+    
+    total_months = 0
+    net_pl = []
+    previous_month = 867884
+    change = 0
+    change_list = []
+    greatest_profit_month = ""
+    most_profit = 0
+    greatest_loss_month = ""
+    most_loss = sys.maxsize
+    average_change = 0
+    total_amount = 0
+      
+    for row in csvreader:
+        current_month = row["Profit/Losses"]
+        change = float(current_month) - float(previous_month) 
+    
+        total_months = total_months + 1
+         
+        Date_find = row["Date"]
+                
+        net_pl.append(int(current_month))
+        total_amount = sum(net_pl)
+             
+        previous_month = row["Profit/Losses"]   
+                
+        if change > most_profit:
+            most_profit = change
+            greatest_profit_month = Date_find
+        if  change < most_loss:
+            most_loss = change
+            greatest_loss_month = Date_find
+        
+        change_list.append(change)  
+            
+average_change=float(sum(change_list)/(len(change_list) - 1))
+
+output = f'''
+    Financial Analysis
+    ----------------------------
+    Total Months: {total_months}
+    Total: #{total_amount:.2f}
+    Average  Change: ${average_change:.2f}
+    Greatest Increase in Profits: {greatest_profit_month} (${most_profit:.2f})
+    Greatest Decrease in Profits: {greatest_loss_month} (${most_loss:.2f})
+    '''
+print(output)
+output_file = os.path.join("PyBankOutput.txt")
+with open(output_file, 'w') as textfile:
+    textfile.write(output)
+
+
+
+    
